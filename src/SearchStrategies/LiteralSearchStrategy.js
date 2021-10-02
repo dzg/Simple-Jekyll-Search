a9162ed401c1cec2c5f867724f21c62ba969df2e
dzg <zgilad@gmail.com>
2021-10-01 17:36:50 -0700

    add search by phrase functionality
    This is working ... except when you add the quote after the fact. Not sure why.

--- src/SearchStrategies/LiteralSearchStrategy.js
+++ src/SearchStrategies/LiteralSearchStrategy.js
@@ -8,6 +8,15 @@ function LiteralSearchStrategy () {
     str = str.trim().toLowerCase()
     crit = crit.endsWith(' ') ? [crit.toLowerCase()] : crit.trim().toLowerCase().split(' ')
 
-    return crit.filter(word => str.indexOf(word) >= 0).length === crit.length
+    // SEARCH BY PHRASE IF SEARCH INPUT STARTS WITH A QUOTE CHAR
+    if (crit.search(/['"“”‘’]/) == 0) {
+      return str.indexOf(crit.replace(/['"“”‘’]/g, '')) >= 0
+    }
+
+    // OTHERWISE SEARCH FOR ALL WORDS
+    return crit.split(' ').filter(function (word) {
+      return str.indexOf(word) >= 0
+    }).length === crit.split(' ').length
   }
 }
+
